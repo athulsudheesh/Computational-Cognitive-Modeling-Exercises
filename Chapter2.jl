b948fc4fb14df956af5006316296a8166da58a2c
Athul Sudheesh <athuls0708@gmail.com>
2020-12-10 23:24:47 -0600

    Comeplete working random walks with paths plotted

--- Chapter2.jl
+++ Chapter2.jl
@@ -14,7 +14,7 @@ rnorm = Normal(drift, std)
 fixed_parms = [drift,std,criterion, rnorm]
 
 
-
+# Random Walk Simulation 
 function random_walk(nreps::Int64,nsamples::Int64, fixed_parms...)
     # Initializing free parameters 
     latencies = fill(0, nreps)
@@ -23,16 +23,27 @@ function random_walk(nreps::Int64,nsamples::Int64, fixed_parms...)
 
     for i in 1:nreps
         evidence[i, :] = accumulate(+,pushfirst!(rand(rnorm, nsamples),0))
-        p = findfirst(abs.(evidence[1,:]) .> drift)
+        p = findfirst(abs.(evidence[i,:]) .> criterion)
         responses[i] = sign(evidence[i,p])
         latencies[i] = p 
     end 
+    return evidence, responses, latencies
 end
 
-##### Plot a few random-walk paths
-random_walk(nreps,nsamples, fixed_parms...)
+## Plot the random walk path 
+function plot_rw_path(evidence, latencies, criterion)
+    plot([evidence[i, 1:(latencies[i])] for i in 1:5],
+        legend = false, 
+        ylims = [-criterion - 0.5, criterion + 0.5], 
+        xlims = maximum(latencies[1:5]), 
+        xlabel = "Decision Time", 
+        ylabel = "Evidence", 
+        grid = false)
+hline!([criterion, -criterion], line =:dash)
+end
 
-plot(evidence[1:5,latencies[1:5] .- 1], 
-    legend =false,
-    ylims = [-criterion - 0.5, criterion + 0.5],
-    xlims = [1, 50])
+
+evidence, responses, latencies = random_walk(nreps,nsamples, fixed_parms...)
+plot_rw_path(evidence, latencies, criterion)
+
+# savefig("random_walk_path.png")

007dc4ce82a7e4000739e9552ffdc05e5d8ab818
Athul Sudheesh <athuls0708@gmail.com>
2020-12-10 21:49:22 -0600

    random walk fixed: fast computation

--- Chapter2.jl
+++ Chapter2.jl
@@ -10,20 +10,29 @@ nsamples = 2000
 drift = 0.0
 std = 0.3
 criterion = 3
-
-# Free Parameters
-latencies = fill(0, nreps)
-responses = fill(0, nreps)
-evidence = zeros(nreps, nsamples+1)
-
-for i in 1:nreps
-    evidence[i, :] = accumulate(+,[0 rand(Normal(drift, std), nsamples)...])
-    p = findfirst(x-> abs(x) > criterion, evidence[i,:])
-    responses[i] = sign(evidence[i,p])
-    latencies[i] = p 
-end 
-
-##### Plot a few randim-walk paths
+rnorm = Normal(drift, std)
+fixed_parms = [drift,std,criterion, rnorm]
 
 
-#plot(evidence[1:100,latencies[1:100] .- 1])
+
+function random_walk(nreps::Int64,nsamples::Int64, fixed_parms...)
+    # Initializing free parameters 
+    latencies = fill(0, nreps)
+    responses = fill(0, nreps)
+    evidence = zeros(nreps, nsamples+1)
+
+    for i in 1:nreps
+        evidence[i, :] = accumulate(+,pushfirst!(rand(rnorm, nsamples),0))
+        p = findfirst(abs.(evidence[1,:]) .> drift)
+        responses[i] = sign(evidence[i,p])
+        latencies[i] = p 
+    end 
+end
+
+##### Plot a few random-walk paths
+random_walk(nreps,nsamples, fixed_parms...)
+
+plot(evidence[1:5,latencies[1:5] .- 1], 
+    legend =false,
+    ylims = [-criterion - 0.5, criterion + 0.5],
+    xlims = [1, 50])
